0e368121c686a2fc3a67e8eab794e386058ce3ff
chad-ramos <jedi_obi_wan_@hotmail.com>
2019-02-19 10:06:04 -0600

    Add test project

---
+++ src/Pioneer.Logs.Console.Tests/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Pioneer.Logs.Tubs.NetCoreConsole;
+
+namespace Pioneer.Logs.Console.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PioneerLogsTub.RegisterLogger();
+
+            try
+            {
+                System.Console.WriteLine("********* DriveSafe LPR API Load Test Mocking *********");
+                System.Console.WriteLine();
+
+                System.Console.Write("Enter number of iterations:");
+                RunPerformanceLoad(int.Parse(System.Console.ReadLine()));
+
+                System.Console.WriteLine();
+                System.Console.WriteLine("Done");
+
+                System.Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e);
+                System.Console.ReadKey();
+                throw;
+            }
+        }
+
+        private static void RunPerformanceLoad(int iterations)
+        {
+            for (var i = 0; i < iterations; i++)
+            {
+                PioneerLogsTub.StartPerformanceTracker(i.ToString());
+                PioneerLogsTub.StopPerformanceTracker();
+            }
+        }
+    }
+}
