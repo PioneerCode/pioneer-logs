ec275062e5ad433f3681dc9de79b88dfc616e730
chad-ramos <jedi_obi_wan_@hotmail.com>
2020-03-12 17:33:06 -0500

    Add 31 project

---
+++ samples/PIoneer.Logs.Samples.AspNetCore.31/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pioneer.Logs.Tubs.AspNetCore;
+
+namespace PIoneer.Logs.Samples.AspNetCore._31
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddPioneerLogs(Configuration.GetSection("PioneerLogsConfiguration"));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UsePioneerLogs();
+        }
+    }
+}
